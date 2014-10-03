9bb5a2db5db63f37d1beb316b80ab01a52b89638
tib <tiborvukovic91@gmail.com>
2014-10-03 21:10:41 +0200

    Add user model test

--- src/test/models/index.js
+++ src/test/models/index.js
@@ -1,33 +1,40 @@
-var mongoose = require('mongoose');
-var mockgoose = require('mockgoose');
-var assert = require('assert');
+var chai = require('chai');
+var should = chai.should();
+var assert = chai.assert;
 
-// mock mongoose
-mockgoose(mongoose);
-
-// fake connect to db
-mongoose.connect('mongodb://localhost/fakedb');
-
-// target
-var models = require('../../models')(mongoose);
-
-describe("User", ()=>{
-    var testUser;
-    before((done)=>{
-        new models.User({
+module.exports = models => {
+    describe("User", ()=> {
+        var testUser = {
             username: "test",
-            password: "test121312313"
-        }).save((err,usr)=>{
-            assert.equal(err,null);
-            testUser = usr;
-            done();
+            password: "prompetiss"
+        };
+        before(done => {
+            new models.User(testUser).save((err, usr)=> {
+                should.not.exist(err);
+                done();
+            });
+        });
+        after(done => {
+            models.User.remove({}, done);
+        });
+        it('should be able to be found in the db', done => {
+            models.User.findOne({username: testUser.username}, (err, usr)=> {
+                should.not.exist(err);
+                should.exist(usr);
+                usr.username.should.equal(testUser.username, 'retrieved username should match the saved username');
+                usr.password.should.not.equal(testUser.password, 'password should be hashed before save');
+                done();git
+            });
+        });
+        it('should be able to validate password', done => {
+            models.User.findOne({username: testUser.username}, (err, usr)=> {
+                usr.validPassword(testUser.password, (err, res)=> {
+                    should.not.exist(err, 'password validation should not have errors');
+                    assert(res, 'password should be valid');
+                    done();
+                });
+            });
         });
     });
-    it("should be found", ()=>{
-        models.User.findOne({username:testUser.username}, (err,usr)=>{
-            assert.equal(err,null);
-            assert.equal(usr.username,testUser.username);
-            assert.equal(usr.password,testUser.password);
-        });
-    })
-});
+};
+
