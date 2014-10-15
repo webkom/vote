8d5d8487b22236fd0dd20eba675f4c069f28af28
tib <tiborvukovic91@gmail.com>
2014-10-15 19:22:02 +0200

    Add vote model

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ exports = module.exports = (mongoose) => {
     return {
         User: require('./user')('user', mongoose),
         Election: require('./election')('election', mongoose),
-        Alternative: require('./alternative')('alternative', mongoose)
+        Alternative: require('./alternative')('alternative', mongoose),
+        Vote: require('./alternative')('vote', mongoose)
     };
 };
--- src/models/vote.js
+++ src/models/vote.js
@@ -1,3 +1,16 @@
-/**
- * Created by tib on 15.10.14.
- */
+exports = module.exports = (collection, mongoose) => {
+    var schema = mongoose.Schema({
+        hash: {
+            type: String,
+            required: true,
+            index: true
+        },
+        alternative: {
+            type: mongoose.Schema.Types.ObjectId, ref: 'alternative',
+            required: true,
+            index: true
+        }
+    });
+
+    return mongoose.model(collection, schema);
+};
