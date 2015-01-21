703cb774d99e106bbc62d5a45133af7a22e7aa7b
tib <tiborvukovic91@gmail.com>
2015-01-21 11:09:44 +0100

    Add test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var server      = require('gulp-express');
 var mocha       = require('gulp-mocha');
+var istanbul    = require('gulp-istanbul');
+
 
 var distDir = 'app/';
 
@@ -10,10 +12,15 @@ gulp.task('server', function () {
     });
 });
 
-
-gulp.task('test', function () {
-    gulp.src(distDir+'test/**/*.js')
-        .pipe(mocha({
-            reporter: 'spec'
-        }));
+gulp.task('test', function (cb) {
+    gulp.src([distDir+'**/*.js',distDir+'server.js'])
+        .pipe(istanbul()) // Covering files
+        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+        .on('finish', function () {
+            gulp.src([distDir +'test/**/*.js'])
+                .pipe(mocha())
+                .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+                .on('end', cb);
+        });
 });
+
