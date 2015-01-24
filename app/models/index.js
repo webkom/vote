8c921b6c31707821b9cf0143cbeebd14c4c3ccfe
ekmartin <ekmartinb@gmail.com>
2015-01-24 17:06:12 +0100

    Make models export themself

--- app/models/index.js
+++
@@ -1,8 +0,0 @@
-exports = module.exports = function (mongoose) {
-    return {
-        User: require('./user')('user', mongoose),
-        Election: require('./election')('election', mongoose),
-        Alternative: require('./alternative')('alternative', mongoose),
-        Vote: require('./vote')('vote', mongoose)
-    };
-};
