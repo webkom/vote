02dfc0e57fa733c6b6d9aeb1a3d4ecf32906ad3c
ekmartin <ekmartinb@gmail.com>
2015-02-03 21:05:02 +0100

    Add bower

--- public/js/controllers/electionCtrl.js
+++ public/js/controllers/electionCtrl.js
@@ -1,5 +1,7 @@
 angular.module('voteApp').controller('electionController', function($scope, apiService) {
 
+    $scope.election = {};
+
     apiService.getElection().then(function(response) {
         var data = response.data;
         $scope.id = data._id;
