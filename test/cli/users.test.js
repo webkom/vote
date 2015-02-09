8492fd86c17686b4e947ff2ce4c3f741d6851a75
ekmartin <ekmartinb@gmail.com>
2015-02-09 18:07:52 +0100

    Take the password in through a prompt instead of as an argument

--- test/cli/users.test.js
+++ test/cli/users.test.js
@@ -1,5 +1,5 @@
 var chai = require('chai');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var User = require('../../app/models/user');
 var should = chai.should();
 
@@ -9,10 +9,18 @@ describe('Users CLI', function() {
     });
 
     it('should create admin users', function(done) {
-        exec(process.cwd() + '/bin/users create-admin testuser pw', function(err, stdout, stderr) {
-            if (err) return done(err);
-            stdout.should.include('Created user testuser');
+        var stream = spawn(process.cwd() + '/bin/users', ['create-admin', 'testuser']);
+        stream.stdin.setEncoding('utf8');
+        stream.stdout.setEncoding('utf8');
+        stream.stdin.write('testpw\n');
 
+        var output = '';
+        stream.stdout.on('data', function(data) {
+            output += data;
+        });
+
+        stream.on('close', function() {
+            output.should.include('Created user testuser');
             User.findOneAsync({ username: 'testuser' })
                 .then(function(user) {
                     user.admin.should.equal(true);
