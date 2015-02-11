c4d625a7c9107352b87ab2606eb8c16965cf4f47
ekmartin <ekmartinb@gmail.com>
2015-02-11 23:49:18 +0100

    Create voting page and fix styling

--- public/js/controllers/electionCtrl.js
+++ public/js/controllers/electionCtrl.js
@@ -1,13 +1,34 @@
 angular.module('voteApp').controller('electionController',
 ['$scope', 'apiService', 'alertService', function($scope, apiService, alertService) {
 
-    $scope.election = null;
+    apiService.getElections()
+    $scope.activeElection = null;
+    $scope.selectedAlternative = null;
+    $scope.voteText = $scope.selectedAlternative ? 'Stem på ' + $scope.selectedAlternative.description : 'Velg et alternativ';
 
-    apiService.getElection()
-        .success(function(election) {
-            $scope.election = election;
+    apiService.getElections()
+        .success(function(elections) {
+            $scope.elections = elections;
+            $scope.elections.some(function(election) {
+                if (election.active) {
+                    $scope.activeElection = election;
+                    return true;
+                }
+            });
         })
         .error(function(err) {
             alertService.addError(err.message);
         });
+
+    $scope.selectAlternative = function(alternative) {
+        $scope.selectedAlternative = alternative;
+    };
+
+    $scope.vote = function() {
+        console.log('voting');
+    };
+
+    $scope.isChosen = function(alternative) {
+        return alternative === $scope.selectedAlternative;
+    };
 }]);
---
+++ public/js/directives/confirmVoteDirective.js
@@ -0,0 +1,33 @@
+angular.module('voteApp').directive('confirmVote', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            selectedAlternative: '=',
+            voteHandler: '&'
+        },
+        template: '' +
+        '<button type="button" ng-click="click()" class="btn btn-lg btn-default">' +
+            '{{buttonText || "Velg et alternativ"}}' +
+        '</button>',
+
+        link: function(scope, elem, attrs) {
+            var clicked = false;
+
+            scope.$watch('selectedAlternative', function(newValue) {
+                if (newValue) {
+                    scope.buttonText = 'Stem på ' + newValue.description;
+                }
+            });
+
+            scope.click = function() {
+                if (!clicked) {
+                    clicked = true;
+                    scope.buttonText = 'Er du sikker?';
+                } else {
+                    scope.voteHandler();
+                }
+            };
+        }
+    };
+});
