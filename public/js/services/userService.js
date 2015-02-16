5bef5c524ed3a5262e22193b2d680d0f18cb8288
ekmartin <ekmartinb@gmail.com>
2015-02-16 21:52:04 +0100

    Add support for showing votes at the edit election page, + styling

--- public/js/services/userService.js
+++ public/js/services/userService.js
@@ -1,11 +1,11 @@
 angular.module('voteApp').service('userService', ['$http', function($http) {
 
     this.activateUser = function(cardKey) {
-        return $http({ method: 'POST', url: '/api/user/' + cardKey + '/toggle_active' });
+        return $http.post('/api/user/' + cardKey + '/toggle_active');
     };
 
     this.createUser = function(user) {
-        return $http({ method: 'POST', data: user, url: '/api/user' });
+        return $http.post('/api/user', user);
     };
 
 }]);
