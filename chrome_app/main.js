9a02a1f7deafbdfea706906873dd3c56ff7aa69a
Kristoffer K Larsen <kristoffer@larsen.so>
2017-03-01 18:39:21 +0100

    Added code for new rfid reader

--- chrome_app/main.js
+++ chrome_app/main.js
@@ -15,6 +15,75 @@ var updateTarget = function(value) {
     wv.contentWindow.postMessage(value, '*');
 };
 
+var commands = {
+  BUZZER: 0x89,
+  MIFARE: {
+    GET_SNR: 0x25
+  }
+};
+
+var replies = {
+  '0': 'OK', // eslint-disable-line
+  '1': 'ERROR', // eslint-disable-line
+  '83': 'NO CARD', // eslint-disable-line
+  '87': 'UNKNOWN INTERNAL ERROR', // eslint-disable-line
+  '85': 'UNKNOWN COMMAND', // eslint-disable-line
+  '84': 'RESPONSE ERROR', // eslint-disable-line
+  '82': 'READER TIMEOUTE', // eslint-disable-line
+  '90': 'CARD DOES NOT SUPPORT THIS COMMAND', // eslint-disable-line
+  '8f': 'UNNSUPPORTED CARD IN NFC WRITE MODE' // eslint-disable-line
+};
+
+var calculateChecksum = function(command, data) => {
+  const payload = [data.length + 1, command, ...data];
+  const checksum = payload.reduce((previousValue, currentValue) => previousValue ^ currentValue);
+  return [...payload, checksum];
+}
+
+var poll = function(connectionId) {
+  var command = this.createMessage(commands.MIFARE.GET_SNR, [0x26, 0x00]);
+  this.writeAndDrain(connectionId, command, poll);
+ }
+
+var writeAndDrain = function(connectionId, data, callback) => {
+  chrome.serial.send(connectionId, data, function (sendInfo) {
+    if (sendInfo.error) {
+      updateStatus('RFID Error:' + sendInfo.error)
+    }
+
+    writeAndDrain(connectionId, data, callback);
+ });
+}
+
+var validate = function(data, checksum) => {
+  const dataDecimal = data.map((item) => parseInt(item, 16));
+  const calculatedChecksum = dataDecimal
+    .reduce((previousValue, currentValue) => previousValue ^ currentValue);
+  return Math.abs(calculatedChecksum % 255) === parseInt(checksum, 16);
+}
+
+var onData = function(response) => {
+  const hexValues = [];
+  for (let i = 0; i < response.length; i += 1) {
+    hexValues.push(response[i].toString(16));
+  }
+  const stationId = hexValues[1];
+  const length = hexValues[2];
+  const status = hexValues[3];
+  const flag = hexValues[4];
+  const data = hexValues.slice(5, hexValues.length - 1);
+  const checksum = hexValues[hexValues.length - 1];
+  const valid = validate([stationId, length, status, flag, ...data], checksum);
+  if (this.replies[status] === 'OK' && this.replies[flag] !== 'NO CARD' && valid) {
+    this.emit('rfid', data.join(':'));
+  }
+}
+
+var createMessage = function(command, data) => {
+  const payload = calculateChecksum(command, data);
+  return new Buffer([0xAA, 0x00, ...payload, 0xBB]);
+}
+
 // Populate the list of available devices
 var refresh = function() {
     serial.getDevices(function(ports) {
@@ -47,13 +116,16 @@ $('#connect_button').addEventListener('click', function() {
 
     serial.connect(devicePath, { bitrate: 9600 }, function(info) {
         updateStatus('connected');
+        var connectionId = info.connectionId;
         var nextRead = Date.now();
         var temp = '';
+        poll();
 
         serial.onReceive.addListener(function(readInfo) {
             if (Date.now() < nextRead) return;
             updateStatus('reading...');
             temp += String.fromCharCode.apply(null, new Uint8Array(readInfo.data));
+            console.log(temp);
             if (temp.length > 11) {
                 updateTarget(temp.substring(0, 11));
                 temp = '';
