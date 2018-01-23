329225a3ba98e06555aeb409a4d7f6fceb80d072
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2018-01-23 02:32:45 +0100

    Listen on 0.0.0.0 if production

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = require('./app');
 app.set('port', process.env.PORT || 3000);
 
 module.exports = function(callback) {
-    var server = app.listen(app.get('port'), 'localhost', function(err) {
+    var hostname = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+    var server = app.listen(app.get('port'), hostname, function(err) {
         callback(err, app.get('port'));
     });
     app.set('io', socketIO(server));
