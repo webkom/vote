334e995c65652a12efd82e37ee453645679bdcfd
Martin Ek <mail@ekmartin.com>
2018-01-29 23:07:44 +0100

    Add a test for /healthz

--- test/api/api.test.js
+++ test/api/api.test.js
@@ -1,4 +1,6 @@
 const chai = require('chai');
+const request = require('supertest');
+const app = require('../../app');
 const { test404 } = require('./helpers');
 
 chai.should();
@@ -10,5 +12,13 @@ describe('API', () => {
 
   it('should return 404 for POSTs to nonexistent API endpoints', async () => {
     await test404('post', '/api/nonexistent', '/api/nonexistent');
+  });
+
+  it('should return 200 for /healthz', async () => {
+    const { body } = await request(app)
+      .get('/healthz')
+      .expect(200);
+
+    body.should.deep.equal({});
   });
 });
