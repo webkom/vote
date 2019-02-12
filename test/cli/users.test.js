011bf702085725ff3754d2fe60fa8d8064c9922c
Peder Smith <smith.peder@gmail.com>
2019-02-12 21:29:08 +0100

    Write new CLI tests for create-user

--- test/cli/users.test.js
+++ test/cli/users.test.js
@@ -3,19 +3,24 @@ const spawn = require('child_process').spawn;
 const User = require('../../app/models/user');
 const should = chai.should();
 
-describe('Users CLI', () => {
+describe('User CLI', () => {
   beforeEach(() => User.remove({}));
 
-  it('should create admin users', done => {
+  it('should create normal user', done => {
     const stream = spawn(`${process.cwd()}/bin/users`, [
-      'create-admin',
-      'testuser',
-      'testcardkey'
+      'create-user',
+      'normaluser',
+      'testcardkey1'
     ]);
 
     stream.stdin.setEncoding('utf8');
     stream.stdout.setEncoding('utf8');
-    stream.stdin.write('testpw\n');
+
+    stream.stdin.write('1\n');
+
+    stream.stdout.on('data', data => {
+      stream.stdin.write('testpassword\n');
+    });
 
     let output = '';
     stream.stdout.on('data', data => {
@@ -23,10 +28,77 @@ describe('Users CLI', () => {
     });
 
     stream.on('close', () => {
-      output.should.include('Created user testuser');
-      User.findOne({ username: 'testuser' })
+      output.should.include('Created user normaluser');
+      User.findOne({ username: 'normaluser' })
+        .then(user => {
+          user.admin.should.equal(false);
+          user.moderator.should.equal(false);
+          should.not.exist(user.password);
+        })
+        .nodeify(done);
+    });
+  });
+
+  it('should create moderator user', done => {
+    const stream = spawn(`${process.cwd()}/bin/users`, [
+      'create-user',
+      'moderator',
+      'testcardkey2'
+    ]);
+
+    stream.stdin.setEncoding('utf8');
+    stream.stdout.setEncoding('utf8');
+
+    stream.stdin.write('2\n');
+
+    stream.stdout.on('data', data => {
+      stream.stdin.write('testpassword\n');
+    });
+
+    let output = '';
+    stream.stdout.on('data', data => {
+      output += data;
+    });
+
+    stream.on('close', () => {
+      output.should.include('Created user moderator');
+      User.findOne({ username: 'moderator' })
+        .then(user => {
+          user.admin.should.equal(false);
+          user.moderator.should.equal(true);
+          should.not.exist(user.password);
+        })
+        .nodeify(done);
+    });
+  });
+
+  it('should create admin user', done => {
+    const stream = spawn(`${process.cwd()}/bin/users`, [
+      'create-user',
+      'admin',
+      'testcardkey3'
+    ]);
+
+    stream.stdin.setEncoding('utf8');
+    stream.stdout.setEncoding('utf8');
+
+    stream.stdin.write('3\n');
+
+    stream.stdout.on('data', data => {
+      stream.stdin.write('testpassword\n');
+    });
+
+    let output = '';
+    stream.stdout.on('data', data => {
+      output += data;
+    });
+
+    stream.on('close', () => {
+      output.should.include('Created user admin');
+      User.findOne({ username: 'admin' })
         .then(user => {
           user.admin.should.equal(true);
+          user.moderator.should.equal(true);
           should.not.exist(user.password);
         })
         .nodeify(done);
