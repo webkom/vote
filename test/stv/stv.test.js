7aa132fd213a97668cdf95cc85dc027cd6c8552a
Peder Smith <smith.peder@gmail.com>
2020-11-28 17:30:34 +0100

    Add dataset converter, datasets and tests for the STV algorithm

---
+++ test/stv/stv.test.js
@@ -0,0 +1,433 @@
+const mongoose = require('mongoose');
+const Alternative = require('../../app/models/alternative');
+const Election = require('../../app/models/election');
+const Vote = require('../../app/models/vote');
+const crypto = require('crypto');
+const chai = require('chai');
+const chaiSubset = require('chai-subset');
+const dataset = require('./datasets');
+
+const should = chai.should();
+chai.use(chaiSubset);
+
+describe('STV Logic', () => {
+  const prepareElection = async function (dataset) {
+    // Takes the priorities from the dataset, as well as the amount of times
+    // that priority combination should be repeated. This basically transforms
+    // the dataset from a reduced format into the format used by the .elect()
+    // method for a normal Vote-STV election. The reason the dataset are written
+    // on the reduced format is for convenience, as it would be to hard to read
+    // datasets with thousands of duplicate lines
+    const repeat = async (priorities, amount) => {
+      const resolvedAlternatives = await Promise.all(
+        priorities.map((d) => Alternative.findOne({ description: d }))
+      );
+      return new Array(amount).fill(resolvedAlternatives);
+    };
+
+    // Step 1) Create Election
+    const election = await Election.create({
+      title: 'Title',
+      description: 'Description',
+      active: true,
+      seats: dataset.seats,
+    });
+    // Step 2) Mutate alternatives and create a new Alternative for each
+    const alternatives = await Promise.all(
+      dataset.alternatives
+        .map((a) => ({
+          election: election._id,
+          description: a,
+        }))
+        .map((a) => new Alternative(a))
+    );
+    // Step 3) Update election with alternatives
+    for (let i = 0; i < alternatives.length; i++) {
+      await election.addAlternative(alternatives[i]);
+    }
+
+    // Step 4) Create priorities from dataset
+    const allPriorities = await Promise.all(
+      dataset.priorities.flatMap((entry) =>
+        repeat(entry.priority, entry.amount)
+      )
+    );
+    // Step 5) Use the resolved priorities to create vote ballots
+    const resolvedVotes = await Promise.all(
+      allPriorities.flat().map((priorities) =>
+        new Vote({
+          hash: crypto.randomBytes(12).toString('hex'),
+          priorities,
+        }).save()
+      )
+    );
+
+    // Set the votes and deactivate the election before saving
+    election.votes = resolvedVotes;
+    election.active = false;
+    await election.save();
+
+    return election;
+  };
+
+  it('should find 2 winners, and resolve for dataset 1', async function () {
+    const election = await prepareElection(dataset.dataset1);
+    const electionResult = await election.elect();
+
+    electionResult.should.containSubset({
+      thr: 34,
+      result: {
+        status: 'RESOLVED',
+        winners: [{ description: 'Andrea' }, { description: 'Carter' }],
+      },
+      log: [
+        {
+          action: 'ITERATION',
+          iteration: 1,
+          winners: [],
+          counts: {
+            Andrea: 45,
+            Brad: 30,
+            Carter: 25,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'Andrea' },
+          voteCount: 45,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 2,
+          winners: [{ description: 'Andrea' }],
+          counts: {
+            Brad: 30,
+            Carter: 36,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'Carter' },
+          voteCount: 36,
+        },
+      ],
+    });
+  });
+
+  it('should find 2 winners, but not resolve for dataset 2', async function () {
+    const election = await prepareElection(dataset.dataset2);
+    const electionResult = await election.elect();
+
+    electionResult.should.containSubset({
+      thr: 6,
+      result: {
+        status: 'UNRESOLVED',
+        winners: [{ description: 'Chocolate' }, { description: 'Orange' }],
+      },
+      log: [
+        {
+          action: 'ITERATION',
+          iteration: 1,
+          winners: [],
+          counts: {
+            Orange: 4,
+            Pear: 2,
+            Chocolate: 12,
+            Strawberry: 1,
+            Hamburger: 1,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'Chocolate' },
+          voteCount: 12,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 2,
+          winners: [{ description: 'Chocolate' }],
+          counts: {
+            Orange: 4,
+            Pear: 2,
+            Strawberry: 5,
+            Hamburger: 3,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'Pear' }],
+          minScore: 2,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 3,
+          winners: [{ description: 'Chocolate' }],
+          counts: {
+            Orange: 6,
+            Strawberry: 5,
+            Hamburger: 3,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'Orange' },
+          voteCount: 6,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 4,
+          winners: [{ description: 'Chocolate' }, { description: 'Orange' }],
+          counts: {
+            Strawberry: 5,
+            Hamburger: 3,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'Hamburger' }],
+          minScore: 3,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 5,
+          counts: {
+            Strawberry: 5,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'Strawberry' }],
+          minScore: 5,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 6,
+          counts: {},
+        },
+      ],
+    });
+  });
+
+  it('should find 4 winners, but not resolve for dataset 3', async function () {
+    const election = await prepareElection(dataset.dataset3);
+    const electionResult = await election.elect();
+
+    electionResult.should.containSubset({
+      thr: 108,
+      result: {
+        status: 'UNRESOLVED',
+        winners: [
+          { description: 'EVANS' },
+          { description: 'STEWART' },
+          { description: 'AUGUSTINE' },
+          { description: 'HARLEY' },
+        ],
+      },
+      log: [
+        {
+          action: 'ITERATION',
+          iteration: 1,
+          winners: [],
+          counts: {
+            STEWART: 66,
+            VINE: 48,
+            AUGUSTINE: 95,
+            COHEN: 55,
+            LENNON: 58,
+            EVANS: 144,
+            WILCOCKS: 60,
+            HARLEY: 91,
+            PEARSON: 30,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'EVANS' },
+          voteCount: 144,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 2,
+          winners: [{ description: 'EVANS' }],
+          counts: {
+            STEWART: 68,
+            VINE: 68,
+            AUGUSTINE: 95,
+            COHEN: 64,
+            LENNON: 58,
+            WILCOCKS: 60,
+            HARLEY: 92,
+            PEARSON: 34,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'PEARSON' }],
+          minScore: 34,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 3,
+          winners: [{ description: 'EVANS' }],
+          counts: {
+            STEWART: 69,
+            VINE: 91,
+            AUGUSTINE: 96,
+            COHEN: 69,
+            LENNON: 58,
+            WILCOCKS: 60,
+            HARLEY: 93,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'LENNON' }],
+          minScore: 58,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 4,
+          winners: [{ description: 'EVANS' }],
+          counts: {
+            STEWART: 115,
+            VINE: 97,
+            AUGUSTINE: 96,
+            COHEN: 71,
+            WILCOCKS: 60,
+            HARLEY: 93,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'STEWART' },
+          voteCount: 115,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 5,
+          winners: [{ description: 'EVANS' }, { description: 'STEWART' }],
+          counts: {
+            VINE: 97,
+            AUGUSTINE: 103,
+            COHEN: 71,
+            WILCOCKS: 60,
+            HARLEY: 93,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'WILCOCKS' }],
+          minScore: 60,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 6,
+          winners: [{ description: 'EVANS' }, { description: 'STEWART' }],
+          counts: {
+            VINE: 104,
+            AUGUSTINE: 135,
+            COHEN: 72,
+            HARLEY: 108,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'AUGUSTINE' },
+          voteCount: 135,
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'HARLEY' },
+          voteCount: 108,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 7,
+          winners: [
+            { description: 'EVANS' },
+            { description: 'STEWART' },
+            { description: 'AUGUSTINE' },
+            { description: 'HARLEY' },
+          ],
+          counts: {
+            VINE: 104,
+            COHEN: 72,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'COHEN' }],
+          minScore: 72,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 8,
+          counts: {
+            VINE: 104,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'VINE' }],
+          minScore: 104,
+        },
+      ],
+    });
+  });
+
+  it('should find 1 winner, and resolve for dataset 4', async function () {
+    const election = await prepareElection(dataset.dataset4);
+    const electionResult = await election.elect();
+
+    electionResult.should.containSubset({
+      thr: 103,
+      result: {
+        status: 'RESOLVED',
+        winners: [{ description: 'Erna Solberg' }],
+      },
+      log: [
+        {
+          action: 'ITERATION',
+          iteration: 1,
+          winners: [],
+          counts: {
+            'Erna Solberg': 88,
+            'Siv Jensen': 44,
+            'Bent Høye': 72,
+          },
+        },
+        {
+          action: 'ELIMINATE',
+          alternatives: [{ description: 'Siv Jensen' }],
+          minScore: 44,
+        },
+        {
+          action: 'ITERATION',
+          iteration: 2,
+          winners: [],
+          counts: {
+            'Erna Solberg': 132,
+            'Bent Høye': 72,
+          },
+        },
+        {
+          action: 'WIN',
+          alternative: { description: 'Erna Solberg' },
+          voteCount: 132,
+        },
+      ],
+    });
+  });
+
+  it('should calculate floating points correctly for dataset5', async function () {
+    const election = await prepareElection(dataset.dataset5);
+    const electionResult = await election.elect();
+    //TODO
+  });
+
+  it('should calculate floating points correctly for dataset6', async function () {
+    const election = await prepareElection(dataset.dataset6);
+    const electionResult = await election.elect();
+    //TODO
+  });
+});
