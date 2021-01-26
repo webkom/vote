876445ddb6cacbd74944152556cde5071a4ae00f
Odin Ugedal <odin@uged.al>
2021-01-26 13:05:58 +0100

    Some changes

--- sync/main.go
+++ sync/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,12 +17,12 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var eventId = flag.Int("event", 0, "Event it to make users from")
-var wsUrl = flag.String("socket-url", "", "Websocket url, full url w/token (look in console on abakus.no)")
-var authCookieVote = flag.String("vote-cookie", "", "Vote cookie including the connect.sid=xasda")
-var csrfToken = flag.String("csrf-token", "", "The token u know")
-var voteEndpoint = flag.String("vote-endpoint", "", "usually ends on /api/user/generate")
-var legoUserEndpont = flag.String("lego-user-endpoint", "", "usually ends on /api/v1/users/")
+var eventId = flag.Int("event", 0, "Event id to make users from. On the format: 123")
+var wsUrl = flag.String("socket-url", "", `Websocket url, full url w/token. Look in console on abakus.no. On the format: "wss://ws.abakus-domain.no/?jwt=long-jwt-here"`)
+var authCookieVote = flag.String("vote-cookie", "", `Vote session cookie. On the format: "connect.sid=xasda"`)
+var csrfToken = flag.String("csrf-token", "", `A csrf token from VOTE. Look in a request. On the format: "RaNdom-String" `)
+var voteEndpoint = flag.String("vote-endpoint", "", `Endpoint to VOTE generate. On the format: "https://vote.abakus-domain.no/api/user/generate"`)
+var legoUserEndpont = flag.String("lego-user-endpoint", "", `Endpoint to users on LEGO. On the format: "https://lego-domain.no/api/v1/users/"`)
 
 type LegoAction struct {
 	Type string `json:"type"`
@@ -72,12 +74,28 @@ type LegoUnregisterAction struct {
 
 func main() {
 	flag.Parse()
+	if len(os.Args) == 1 {
+		flag.Usage()
+		os.Exit(2)
+	}
 
 	if len(*wsUrl) == 0 {
 		log.Fatal("Missing wsUrl")
 	}
 	if *eventId == 0 {
 		log.Fatal("Missing eventId")
+	}
+	if len(*authCookieVote) == 0 {
+		log.Fatal("Missing authCookieVote")
+	}
+	if len(*csrfToken) == 0 {
+		log.Fatal("Missing csrfToken")
+	}
+	if len(*voteEndpoint) == 0 {
+		log.Fatal("Missing authCookieVote")
+	}
+	if len(*legoUserEndpont) == 0 {
+		log.Fatal("Missing legoUserEndpoint")
 	}
 
 	log.SetFlags(0)
@@ -87,11 +105,25 @@ func main() {
 
 	u, _ := url.Parse(*wsUrl)
 	jwt := u.Query()["jwt"][0]
-	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	for {
+		err := runLegoToVoteSync(interrupt, u, jwt)
+
+		// Exit if runLegoToVoteSync returns nil, otherwise reconnect
+		if err == nil {
+			break
+		}
+		log.Printf("connection error, will reconnect: %e\n", err)
+		time.Sleep(2 * time.Second)
+	}
+
+}
+func runLegoToVoteSync(interrupt chan os.Signal, url *url.URL, jwt string) error {
+	log.Printf("connecting to %s", url.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		return fmt.Errorf("dial: %e", err)
 	}
 	defer c.Close()
 
@@ -108,13 +140,18 @@ func main() {
 			var abstractAction LegoAction
 			err = json.Unmarshal(message, &abstractAction)
 			if err != nil {
-				log.Printf("Error decding msg: %q with err: %e\n", message, err)
+				log.Printf("Error decoding msg: %q with err: %e\n", message, err)
 				continue
 			}
 			switch abstractAction.Type {
 			case "Event.SOCKET_REGISTRATION.SUCCESS":
 				var action LegoRegisterAction
-				_ = json.Unmarshal(message, &action)
+				err = json.Unmarshal(message, &action)
+
+				if err != nil {
+					log.Printf("Error when decoding action: %e\n", err)
+					break
+				}
 
 				if action.Meta.EventID != *eventId {
 					break
@@ -123,7 +160,14 @@ func main() {
 				log.Printf("Registering user %q in VOTE\n", action.Payload.User.FullName)
 				client := &http.Client{}
 
-				legoGet, _ := http.NewRequest("GET", *legoUserEndpont+action.Payload.User.Username, nil)
+				ctx, cnl := context.WithTimeout(context.Background(), 30*time.Second)
+				defer cnl()
+
+				legoGet, err := http.NewRequestWithContext(ctx, "GET", *legoUserEndpont+action.Payload.User.Username, nil)
+				if err != nil {
+					log.Printf("Error when creating lego user req: %e\n", err)
+					break
+				}
 				legoGet.Header.Add("Authorization", "JWT "+jwt)
 
 				resp, err := client.Do(legoGet)
@@ -131,33 +175,57 @@ func main() {
 					log.Printf("Error when fetching lego user: %e\n", err)
 					break
 				}
-				var voteFormData struct {
+				var legoUserData struct {
 					Email string `json:"email"`
 				}
 				defer resp.Body.Close()
-				respData, _ := ioutil.ReadAll(resp.Body)
-
-				log.Printf("Got this from LEGO: %q\n", respData)
-				err = json.Unmarshal(respData, &voteFormData)
-
+				respData, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-					log.Printf("Error when fetching lego user: %e\n", err)
+					log.Printf("Error when reading lego user: %e\n", err)
 					break
 				}
 
-				out, _ := json.Marshal(voteFormData)
+				err = json.Unmarshal(respData, &legoUserData)
+
+				if err != nil {
+					log.Printf("Error when unmarshalling user: %e\n", err)
+					break
+				}
+
+				voteFormData := struct {
+					Email    string `json:"email"`
+					LegoUser string `json:"legoUser"`
+				}{
+					Email:    legoUserData.Email,
+					LegoUser: action.Payload.User.Username,
+				}
+
+				out, err := json.Marshal(voteFormData)
+				if err != nil {
+					log.Printf("Error when marshalling user: %e\n", err)
+					break
+				}
+
 				log.Printf("Posting to VOTE: %q", string(out))
-				req, _ := http.NewRequest("POST", *voteEndpoint, bytes.NewBuffer(out))
+				req, err := http.NewRequest("POST", *voteEndpoint, bytes.NewBuffer(out))
+				if err != nil {
+					log.Printf("Error when creating VOTE req: %e\n", err)
+					break
+				}
 				req.Header.Add("CSRF-Token", *csrfToken)
 				req.Header.Add("content-type", "application/json;charset=UTF-8")
 				req.Header.Set("Cookie", *authCookieVote)
 
 				resp, err = client.Do(req)
+				if err != nil {
+					log.Printf("Error when fetching VOTE: %e\n", err)
+					break
+				}
 
 				defer resp.Body.Close()
-				respData, _ = ioutil.ReadAll(resp.Body)
+				respData, err = ioutil.ReadAll(resp.Body)
 				if err != nil {
-					log.Printf("Error when creating user: %e, got: %s\n", err, respData)
+					log.Printf("Error when reading from vote: %e\n", err)
 					break
 				}
 				log.Printf("Creating user returned: %s, %s\n", resp.Status, respData)
@@ -184,13 +252,13 @@ func main() {
 	for {
 		select {
 		case <-done:
-			return
+			return fmt.Errorf("unexpected disconnect")
 		case t := <-ticker.C:
 			// Keepalive by chatting a bit I guess
 			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
 			if err != nil {
 				log.Println("write:", err)
-				return
+				return err
 			}
 		case <-interrupt:
 			log.Println("interrupt")
@@ -198,13 +266,14 @@ func main() {
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
-				return
+				return nil
 			}
 			select {
 			case <-done:
 			case <-time.After(time.Second):
 			}
-			return
+			return nil
 		}
 	}
+
 }
