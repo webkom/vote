8781042903c7fffe14baa9f40f9935fe6f3a717b
Ludvig Hozman <ludvig123@gmail.com>
2021-10-02 19:55:28 +0200

    Upgrade webpack to v5 & add ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   mode: 'none',
   output: {
-    path: `${__dirname}/public`,
+    path: path.resolve(__dirname, 'public'),
     filename: '[name].js',
     publicPath: '/static/',
   },
   resolve: {
-    extensions: ['.js', '.styl'],
+    extensions: ['.js', '.styl', '.ts'],
   },
   entry: {
     bundle: './client/app.js',
     login: './client/login.js',
+    server: './index.ts',
   },
   module: {
     rules: [
       {
         test: /\.styl$/,
-        loader: 'file-loader?name=[name].css!stylus-loader',
+        use: [
+          { loader: 'file-loader', options: { name: '[name].css' } },
+          { loader: 'stylus-loader' },
+        ],
       },
       {
         test: /\.mp3$/,
-        loader: 'file-loader?name=[name].mp3',
+        use: [{ loader: 'file-loader', options: { name: '[name].mp3' } }],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
