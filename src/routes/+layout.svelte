b4ee3cbdb7366fc2d8e08f9c7e0647053ddec4b1
PeterJFB <peterj.flbj@gmail.com>
2023-02-07 21:13:47 +0100

    Styling proof of concept

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,246 @@
+<script lang="ts">
+  import logo from '$lib/assets/Abakule.jpg';
+
+  let showLogout = true;
+</script>
+
+<!-- <html ng-app="voteApp"> -->
+
+<header>
+  <div class="container">
+    <div class="row header">
+      <div class="col-xs-12">
+        <img src={logo} alt="abakus logo " /><span>vote</span>
+      </div>
+    </div>
+    <div class="row" />
+  </div>
+</header>
+<slot />
+<footer>
+  <div class="container text-center">
+    {#if showLogout}
+      <span id="logoutSpan">
+        <a href="#top" tabindex="-1">Logg ut</a>
+        | Laget av
+      </span>
+    {/if}
+    <a href="http://github.com/webkom/vote">Webkom</a>
+    | For <a href="https://abakus.no">Abakus</a>
+    | Se <a href="/usage" tabindex="-1" target="_blank">bruk</a>
+    | vote:
+    <a
+      href={'https://hub.docker.com/r/abakus/vote/tags?page=1&ordering=last_updated&name=' +
+        process.env.RELEASE}>{process.env.RELEASE}</a
+    >
+  </div>
+</footer>
+
+<style type="text/scss" lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Raleway:300,200,100');
+  @import url('https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap');
+
+  $font-gray: #666c77;
+  $alternative-background: darken($font-gray, 20%);
+  $dark-gray: #333;
+  $abakus-light: #cc0000;
+  $abakus-dark: #bd0005;
+  $red: #b11b11;
+  $white: #fdfdfd;
+
+  html {
+    min-height: 100%;
+    position: relative;
+  }
+  body {
+    font-family: 'Raleway', 'Lucida Grande';
+    font-weight: 300;
+    word-break: break-word;
+    margin-bottom: 30px;
+    color: $font-gray;
+  }
+  header {
+    width: 100%;
+    border-bottom: 2px solid #e0e0e0;
+    margin-bottom: 20px;
+  }
+  header nav ul {
+    padding-top: 10px;
+    padding-bottom: 20px;
+    text-align: center;
+    width: 100%;
+    margin: 0 auto;
+  }
+  header nav ul li {
+    display: inline;
+  }
+  header nav ul li:not(:first-child) {
+    padding-left: 20px;
+  }
+  header nav ul li a {
+    font-size: 20px;
+    color: $font-gray;
+  }
+  header nav ul li a:hover {
+    color: $abakus-light;
+  }
+  header nav ul li a:focus {
+    text-decoration: none;
+  }
+  .header {
+    text-align: center;
+    font-size: 60px;
+    font-weight: 100;
+    padding-top: 50px;
+  }
+  .header img {
+    margin-right: 15px;
+    width: 35px;
+  }
+  footer {
+    position: absolute;
+    height: 30px;
+    width: 100%;
+    margin: 0 auto;
+    bottom: 0;
+  }
+  footer a {
+    color: $abakus-light;
+  }
+  footer a:hover {
+    color: $abakus-dark;
+  }
+  a {
+    color: $abakus-light;
+  }
+  a:hover,
+  a:focus {
+    color: $abakus-dark;
+    text-decoration: none;
+  }
+  h2,
+  h3,
+  .btn {
+    font-weight: 300;
+  }
+  hr {
+    margin: 10px 0;
+  }
+  .alert {
+    margin: 15px auto;
+    max-width: 90%;
+  }
+  .text-danger {
+    font-size: 18px;
+  }
+  .container {
+    width: 100%;
+  }
+  .center {
+    max-width: 750px;
+    margin: 0 auto;
+    font-size: 21px;
+  }
+  .mono {
+    font-family: 'Cutive Mono', monospace;
+    font-size: 16px;
+  }
+  label {
+    font-weight: 300;
+    font-size: 20px;
+    letter-spacing: 0.1em;
+  }
+  .fade {
+    transition: opacity 0.5s;
+    opacity: 0;
+  }
+  .btn {
+    font-size: 20px;
+  }
+  .btn:active:focus {
+    outline: none;
+  }
+  .btn:focus {
+    outline: none;
+  }
+  .vote-result-form {
+    margin: 0 auto;
+  }
+  .vote-result-form input {
+    margin: 10px auto;
+    text-align: center;
+    width: 70%;
+  }
+  .vote-result-feedback {
+    margin-top: 30px;
+  }
+  .vote-result-feedback p {
+    font-size: 18px;
+  }
+  .form-control {
+    font-size: 18px;
+    height: 40px;
+  }
+  .hidden {
+    display: none;
+  }
+  @media (max-width: 1000px) {
+    .vote-result-form input {
+      width: 100%;
+    }
+    .header {
+      padding-top: 0;
+    }
+    footer {
+      padding-top: 20px;
+    }
+  }
+  .usage-flex {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 10px;
+  }
+  .usage-flex a {
+    align-self: flex-end;
+  }
+  .th-right {
+    text-align: right;
+    width: 50%;
+  }
+  .th-left {
+    text-align: left;
+    width: 50%;
+  }
+  .cs-tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted #000;
+  }
+  .cs-tooltip .cs-tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    bottom: 130%;
+    left: 50%;
+    margin-left: -60px;
+    background-color: #000;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    position: absolute;
+    z-index: 1;
+  }
+  .cs-tooltip .cs-tooltiptext::after {
+    content: ' ';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #000 transparent transparent transparent;
+  }
+  .cs-tooltip:hover .cs-tooltiptext {
+    visibility: visible;
+  }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,38 @@
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-4 col-sm-offset-4 col-md-4 text-center">
+      <video
+        id="testing"
+        muted="muted"
+        playsinline="playsinline"
+        style="width: 100%"
+      /><br />
+      <form action="/auth/login" method="post" role="form">
+        <input type="hidden" name="_csrf" />
+        <div class="form-group">
+          <label for="username">Brukernavn:</label>
+          <input class="form-control" name="username" type="text" />
+        </div>
+        <div class="form-group">
+          <label for="password">Passord:</label>
+          <input class="form-control" name="password" type="password" />
+        </div>
+        <input class="hidden" name="usingToken" value="false" />
+        <div class="hidden" id="alertInfo">
+          <img id="qrImg" width="50%" />
+          <div class="alert alert-danger">
+            <p><b>Velkommen til VOTE</b></p>
+            <p>
+              Ta <b>screenshot</b> av denne siden for å lagre brukernavn og passord
+            </p>
+          </div>
+        </div>
+        <button class="btn btn-default" type="submit">Logg på</button>
+      </form>
+      <br />
+      <div class="hidden" id="confirmScreenshot">
+        <button class="btn btn-default">Jeg har tatt screenshot</button>
+      </div>
+    </div>
+  </div>
+</div>
