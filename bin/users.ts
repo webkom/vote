ce769d5c377a9cc6b599f180e796f90512c0c7f1
Ludvig Hozman <ludvig123@gmail.com>
2023-02-17 15:07:06 +0100

    Add cli options for password & mode to user cli
    This makes testing alot easier. Also a nice to have.

--- bin/users.ts
+++ bin/users.ts
@@ -7,6 +7,7 @@ import mongoose, { HydratedDocumentFromSchema } from 'mongoose';
 import chalk from 'chalk';
 import promptly from 'promptly';
 import User, { userSchema } from '../app/models/user';
+import { parseInt } from 'lodash';
 const program = new Command();
 
 function done(err: Error, user: HydratedDocumentFromSchema<typeof userSchema>) {
@@ -31,35 +32,47 @@ function validator(value: string) {
 program
   .version('1.0.0')
   .command('create-user <username> <cardKey>')
+  .option('-m, --mode <mode>', 'The mode for the user. [1|2|3]')
+  .option('-p, --password <password>', 'password for user')
   .description('create a new user')
-  .action((username, cardKey) => {
+  .action(async (username, cardKey, options) => {
     const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/vote';
 
-    promptly.prompt(
-      'Usermode: \n [1] for User \n [2] for Moderator \n [3] for Admin \n Enter mode: ',
-      { validator, retry: true },
-      (modeErr, mode) => {
-        const modeId = parseInt(mode);
-        mongoose.set('strictQuery', true);
-        mongoose.connect(mongoURL, {}, (connectErr) => {
-          if (connectErr) return done(connectErr, null);
+    const mongoDbName =
+      process.env.NODE_ENV == 'test' && process.env.VITEST_WORKER_ID
+        ? `vote-test-${process.env.VITEST_WORKER_ID}`
+        : null;
 
-          promptly.password('Enter password: ', async (pwErr, password) => {
-            if (pwErr) return done(pwErr, null);
-            const user = new User({
-              username: username,
-              admin: modeId == 3,
-              moderator: modeId == 2 || modeId == 3,
-              cardKey: cardKey,
-            });
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            const registeredUser = await User.register(user, password);
-            done(null, registeredUser);
-          });
-        });
-      }
-    );
+    let { mode, password } = options;
+
+    if (!['1', '2', '3'].includes(mode)) {
+      mode = await promptly.prompt(
+        'Usermode: \n [1] for User \n [2] for Moderator \n [3] for Admin \n Enter mode: ',
+        { validator, retry: true }
+      );
+    }
+
+    const modeId = parseInt(mode);
+
+    if (!(password?.length > 0)) {
+      password = await promptly.password('Enter password: ');
+    }
+
+    mongoose.set('strictQuery', true);
+    mongoose.connect(mongoURL, { dbName: mongoDbName }, async (connectErr) => {
+      if (connectErr) return done(connectErr, null);
+
+      const user = new User({
+        username: username,
+        admin: modeId == 3,
+        moderator: modeId == 2 || modeId == 3,
+        cardKey: cardKey,
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const registeredUser = await User.register(user, password);
+      done(null, registeredUser);
+    });
   });
 
 program.parse(process.argv);
