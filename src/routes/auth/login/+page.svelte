63655fcae6f1bf6b6d394b93f831bacbd6917aac
Jenny Müller <jenny.mul@outlook.com>
2024-03-19 20:28:22 +0100

    Add cool summer features

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -9,6 +9,7 @@
     generateXSRFToken,
   } from '$lib/utils/callApi';
   import { alerts } from '$lib/stores';
+  import p72 from '$lib/assets/72.png';
 
   let username: string = '';
   let password: string = '';
@@ -103,6 +104,7 @@
 </script>
 
 <div class="container">
+  <img src={p72} />
   <div class="row justify-content-center">
     <div class="col-md-6 text-center">
       <video class="center" bind:this={camera} muted playsinline />
